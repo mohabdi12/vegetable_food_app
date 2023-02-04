5145a746a7bd1edfaa076405ee9f9e52f05f7b20
mohabdi12 <mohamedabdisalan702@gmail.com>
2023-02-04 03:41:04 -0800

    my project commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\fluttersrc\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Eng Suleyman ALI\Desktop\dj flutter\flutterui-curveddesigns-master\flutterui-curveddesigns-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
